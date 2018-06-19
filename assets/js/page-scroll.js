f3ac8e1414e7627c857dbee8e8529e26a0a3af64
Anthony Mendez <anthonymendez9@gmail.com>
2018-06-19 18:18:52 -0400

    Made top bar static.

--- assets/js/page-scroll.js
+++ assets/js/page-scroll.js
@@ -1,12 +1,16 @@
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(document).ready(function() {
-    
-    // $('a.page-scroll').bind('click', function(event) {
-    //     var $anchor = $(this);
-    //     $('html, body').stop().animate({
-    //         scrollTop: $($anchor.attr('href')).offset().top
-    //     }, 1500, 'easeInOutExpo');
-    //     event.preventDefault();
-    // });
     $('#navmenulinks').localScroll({duration:800});
+});
+
+$(document).ready(function(){
+    var topPadding = 
+        // parseInt($('.site-header').css('padding-top'), 10) + 
+        // parseInt($('.site-header').css('margin-top'), 10) +
+        // parseInt($('.site-header').css('padding-bottom'), 10) + 
+        // parseInt($('.site-header').css('margin-bottom'), 10) +
+        parseInt($('.site-header').css('height'), 10) + "px";
+    $('.content').css('padding-top', topPadding);
+    // var boxsizing = $('.site-header').css('box-sizing');
+    // $('.content').css('box-sizing', boxsizing);
 });
